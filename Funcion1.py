2bed54be8e25f69f8427fea9ff00582ad16a7e0a
JefriRodriguez <3133734294jefri@gmail.com>
2025-10-30 00:17:05 +0000

    Add funcion1

---
+++ Funcion1.py
@@ -0,0 +1,8 @@
+#sin parametros y sin retornos
+def suma():
+    a=float(input("ingrese el primer numero: "))
+    b=float(input("ingrese el numero a sumar: "))
+    print(f"La suma es {a+b}")
+    
+suma()
+#para validar y mostrar cosas por lo general en los menus
